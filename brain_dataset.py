7fcf4a262dd015cd6c0cd3a735b85a3db4550f3c
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-09-30 21:02:27 +0000

    created custom dataset class for images; updated requirements file

--- brain_dataset.py
+++ brain_dataset.py
@@ -1,5 +1,31 @@
-from glob import glob
+from PIL import Image
 from torch.utils.data import Dataset
-import json
 import os
 
+
+class BrainDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.image_folder = os.path.join(root_dir, "images")
+        self.mask_folder = os.path.join(root_dir, "masks")
+        self.image_files = sorted(os.listdir(self.image_folder))
+        self.mask_files = sorted(os.listdir(self.mask_folder))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_name = self.image_files[idx]
+        img_path = os.path.join(self.image_folder, img_name)
+        image = Image.open(img_path).convert("RGB")
+        image_gray = image.convert("L")  # Convert to grayscale
+        mask_name = self.mask_files[idx]
+        mask_path = os.path.join(self.mask_folder, mask_name)
+        mask = Image.open(mask_path).convert("L")
+
+        if self.transform:
+            # Apply transformations
+            image_gray = self.transform(image_gray)
+            mask = self.transform(mask)
+        return image_gray, mask
