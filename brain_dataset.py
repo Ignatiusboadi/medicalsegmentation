902ea94c7cd995c161819504e0ddc96e2110b823
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-10-01 23:11:57 +0000

    modified custom dataset class to suit new preprocessed data and training functions

--- brain_dataset.py
+++ brain_dataset.py
@@ -5,29 +5,29 @@ import os
 
 
 class BrainDataset(Dataset):
-    def __init__(self, root_dir, image_transform=None, mask_transform=None):
+    def __init__(self, root_dir, img_dir, mask_dir, transform=None):
         self.root_dir = root_dir
-        self.image_folder = os.path.join(root_dir, "images")
-        self.mask_folder = os.path.join(root_dir, "masks")
-        self.image_files = sorted(os.listdir(self.image_folder))
-        self.mask_files = sorted(os.listdir(self.mask_folder))
-        self.image_transform = image_transform
-        self.mask_transform = mask_transform
+        self.transform = transform
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.img_files = [f for f in os.listdir(self.img_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
+        self.mask_files = [f for f in os.listdir(self.mask_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.mask_files)
 
     def __getitem__(self, idx):
-        img_name = self.image_files[idx]
-        img_path = os.path.join(self.image_folder, img_name)
-        image = Image.open(img_path).convert("RGB")
-        image_gray = np.array(image.convert("L")).astype(float)
-        mask_name = self.mask_files[idx]
-        mask_path = os.path.join(self.mask_folder, mask_name)
-        mask = Image.open(mask_path).convert("L")
-        if self.image_transform:
-            image_gray = self.image_transform(image_gray)
+        img_name = self.img_files[idx]
+        img_path = os.path.join(self.img_dir, img_name)
+        img = Image.open(img_path).convert('RGB')
+        img_gray = img.convert('L')
 
-        if self.mask_transform:
-            mask = self.mask_transform(mask)
-        return image_gray, mask
+        mask_name = self.mask_files[idx]
+        mask_path = os.path.join(self.mask_dir, mask_name)
+        mask = Image.open(mask_path).convert('L')
+
+        if self.transform:
+            img_gray = self.transform(img_gray)
+            mask = self.transform(mask)
+
+        return img_gray, mask
