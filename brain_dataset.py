0aafeea5cf46c51780ae5e363366963c9ffc913d
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-10-06 17:30:24 +0000

    modified code to cater for data without masks in production

--- brain_dataset.py
+++ brain_dataset.py
@@ -5,16 +5,17 @@ import os
 
 
 class BrainDataset(Dataset):
-    def __init__(self, root_dir, img_dir, mask_dir, transform=None):
+    def __init__(self, root_dir, img_dir, mask_dir=None, transform=None):
         self.root_dir = root_dir
         self.transform = transform
         self.img_dir = img_dir
         self.mask_dir = mask_dir
         self.img_files = [f for f in os.listdir(self.img_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
-        self.mask_files = [f for f in os.listdir(self.mask_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
+        if mask_dir:
+            self.mask_files = [f for f in os.listdir(self.mask_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
     def __len__(self):
-        return len(self.mask_files)
+        return len(self.img_files)
 
     def __getitem__(self, idx):
         img_name = self.img_files[idx]
@@ -22,12 +23,17 @@ class BrainDataset(Dataset):
         img = Image.open(img_path).convert('RGB')
         img_gray = img.convert('L')
 
-        mask_name = self.mask_files[idx]
-        mask_path = os.path.join(self.mask_dir, mask_name)
-        mask = Image.open(mask_path).convert('L')
+        if self.mask_dir:
+            mask_name = self.mask_files[idx]
+            mask_path = os.path.join(self.mask_dir, mask_name)
+            mask = Image.open(mask_path).convert('L')
 
         if self.transform:
             img_gray = self.transform(img_gray)
-            mask = self.transform(mask)
+            if self.mask_dir:
+                mask = self.transform(mask)
+                output = img_gray, mask
+            else:
+                output = img_gray, None
 
-        return img_gray, mask
+        return output
