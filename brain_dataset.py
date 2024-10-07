1c9a36525a1992c953146d813ea4045e07ab0e90
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-10-07 04:43:14 +0000

    created a production class alongside the training class of brain_dataset

--- brain_dataset.py
+++ brain_dataset.py
@@ -1,18 +1,14 @@
 from PIL import Image
 from torch.utils.data import Dataset
-import numpy as np
 import os
 
 
-class BrainDataset(Dataset):
-    def __init__(self, root_dir, img_dir, mask_dir=None, transform=None):
+class ProdBrainDataset(Dataset):
+    def __init__(self, root_dir, img_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
         self.img_dir = img_dir
-        self.mask_dir = mask_dir
         self.img_files = [f for f in os.listdir(self.img_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
-        if mask_dir:
-            self.mask_files = [f for f in os.listdir(self.mask_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
     def __len__(self):
         return len(self.img_files)
@@ -23,24 +19,37 @@ class BrainDataset(Dataset):
         img = Image.open(img_path).convert('RGB')
         img_gray = img.convert('L')
 
+        if self.transform:
+            img_gray = self.transform(img_gray)
+
+        return img_gray
+
+    def get_original_size(self, idx):
+        img_name = self.img_files[idx]
+        img_path = os.path.join(self.img_dir, img_name)
+        img = Image.open(img_path)
+        return img.size
+
+
+class BrainDataset(ProdBrainDataset):
+    def __init__(self, root_dir, img_dir, mask_dir=None, transform=None):
+        super().__init__(root_dir, img_dir, transform)
+        self.mask_dir = mask_dir
+        if mask_dir:
+            self.mask_files = [f for f in os.listdir(self.mask_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
+
+    def __getitem__(self, idx):
+        img_gray = super().__getitem__(idx)
+
         if self.mask_dir:
             mask_name = self.mask_files[idx]
             mask_path = os.path.join(self.mask_dir, mask_name)
             mask = Image.open(mask_path).convert('L')
 
-        if self.transform:
-            img_gray = self.transform(img_gray)
-            if self.mask_dir:
+            if self.transform:
                 mask = self.transform(mask)
-                output = img_gray, mask
-            else:
-                output = img_gray, None
+            return img_gray, mask
 
-        return output
+        return img_gray, None
 
-    def get_original_size(self, idx):
-        """Return the original size (width, height) of the image."""
-        img_name = self.img_files[idx]
-        img_path = os.path.join(self.img_dir, img_name)
-        img = Image.open(img_path)
-        return img.size
+
