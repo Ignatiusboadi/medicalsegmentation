8a0508cff61d9796a774d84e1d831ac5fda5cebd
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-10-09 19:54:05 +0000

    added frontend codes and grouped backend and frontend codes into different folders

---
+++ fast-api-backend/main.py
@@ -0,0 +1,292 @@
+from brain_dataset import ProdBrainDataset
+from fastapi import FastAPI, File, UploadFile, Depends, HTTPException, status, Header
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.responses import StreamingResponse, FileResponse, HTMLResponse
+from google.cloud import storage
+from datetime import datetime, timedelta, timezone
+from typing import Union
+from jwt import PyJWTError
+from passlib.context import CryptContext
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+import cv2
+from datetime import datetime
+import os
+import zipfile
+import json
+import jwt
+import numpy as np
+import pandas as pd
+import shutil
+import segmentation_models_pytorch as smp
+import torch
+import warnings
+import yagmail
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset, DataQualityPreset
+from evidently.metrics.base_metric import generate_column_metrics
+from evidently.metrics import *
+from evidently.test_suite import TestSuite
+from evidently.tests.base_test import generate_column_tests
+from evidently.test_preset import DataStabilityTestPreset, NoTargetPerformanceTestPreset, RegressionTestPreset
+from evidently.tests import *
+import shutil
+from evidently import ColumnMapping
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+cred = 'focus-surfer-435213-g6.json'
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = cred
+
+
+def clamp_tensor(x):
+    return x.clamp(0, 1)
+
+
+def upload_to_gcp(source_file_name, destination_folder):
+    bucket_name = 'brain-scan-data'
+    destination_blob_name = f'{destination_folder}/{source_file_name}'
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+
+def draw_mask_border(image, mask_generated):
+    image = cv2.imread(image)
+    mask_generated = cv2.imread(mask_generated)
+    if len(mask_generated.shape) == 3:
+        gray_mask = cv2.cvtColor(mask_generated, cv2.COLOR_BGR2GRAY)
+    else:
+        gray_mask = mask_generated
+
+    _, binary_mask = cv2.threshold(gray_mask, 1, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    bordered_image = image.copy()
+    cv2.drawContours(bordered_image, contours, -1, (0, 0, 255), 2)
+
+    return bordered_image
+
+
+########################################################################################
+# TOKEN AUTHENTICATION
+########################################################################################
+SECRET_KEY = "fdb3e44ba75f4d770ee8de98e488bc3ebcf64dc3066c8140a1ae620c30964454"  # Replace with your own secret key
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+users_db = {
+    "admin": {"username": "admin", "password": pwd_context.hash("adminpass"), "role": "admin",
+              'email': 'ignatiusboadi@gmail.com'},
+    "user": {"username": "user", "password": pwd_context.hash("userpass"), "role": "user",
+             'email': 'iboadi@aimsammi.org'},
+    "leo": {"username": "leo", "password": pwd_context.hash("l001"), "role": "user", "email": "lsanya@aimsammi.org"},
+    "milli": {"username": "milli", "password": pwd_context.hash("m001"), "role": "user",
+              "email": 'momondi@aimsammi.org'}
+}
+
+upload_dir = 'uploads'
+root_dir = '/'
+os.makedirs(upload_dir, exist_ok=True)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(username: str, password: str):
+    user = users_db.get(username)
+    if not user or not verify_password(password, user["password"]):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        return username
+    except PyJWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"message": "Welcome to the Image Segmentation using FastAPI app!"}
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    email = user['email']
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user["username"]}, expires_delta=access_token_expires
+    )
+
+    yag = yagmail.SMTP('ammi.mlops.group1@gmail.com', 'pktwlpqogrkotiyg')
+    message = f'''Dear User,
+                        Kindly find below the bearer token for you to access
+                        {access_token}
+
+                        Kindest regards,
+                        Group 1'''
+    yag.send(email, f'Bearer token', message)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+#######################################################################################
+#DATA DRIFT DETECTION
+#######################################################################################
+@app.post("/Drift Monitoring")
+async def data_drift_and_test(token: str = Depends(oauth2_scheme)):
+    decode_token(token)
+    train_json = 'train_annotations.coco.json'
+    test_json = 'test_annotations.coco.json'
+
+    with open(train_json, 'r') as f:
+        train_data = json.load(f)
+    with open(test_json, 'r') as f:
+        test_data = json.load(f)
+
+    train_images = pd.DataFrame(train_data['images'])
+    test_images = pd.DataFrame(test_data['images'])
+
+    column_mapping = ColumnMapping(
+        target=None,
+        categorical_features=[],
+        numerical_features=['width', 'height']
+    )
+
+    report = Report(metrics=[
+        DataDriftPreset(),
+        DataQualityPreset(),
+        ColumnSummaryMetric(column_name='height'),
+        generate_column_metrics(ColumnQuantileMetric, parameters={'quantile': 0.25}, columns=['id']),
+        ColumnDriftMetric(column_name='width')
+    ])
+    report.run(reference_data=train_images, current_data=test_images)
+
+    html_report_path = os.path.join(upload_dir, 'data_drift_report.html')
+    report.save_html(html_report_path)
+
+    tests = TestSuite(tests=[
+        TestNumberOfColumnsWithMissingValues(),
+        TestNumberOfRowsWithMissingValues(),
+        TestNumberOfConstantColumns(),
+        TestNumberOfDuplicatedRows(),
+        TestNumberOfDuplicatedColumns(),
+        TestColumnsType(),
+        TestNumberOfDriftedColumns(),
+    ])
+
+    tests.run(reference_data=train_images, current_data=test_images)
+    html_tests_path = os.path.join(upload_dir, 'data_tests.html')
+    tests.save_html(html_tests_path)
+
+    zip_file_path = os.path.join(upload_dir, 'report_bundle.zip')
+    with zipfile.ZipFile(zip_file_path, 'w') as zipf:
+        zipf.write(html_report_path, os.path.basename(html_report_path))
+        zipf.write(html_tests_path, os.path.basename(html_tests_path))
+
+    return FileResponse(zip_file_path, media_type='application/zip', filename='report_bundle.zip')
+
+
+#######################################################################################
+# PREDICTION ENDPOINT
+#######################################################################################
+@app.post("/prediction")
+async def image_segmentation(file: UploadFile = File(...), token: str = Depends(oauth2_scheme)):
+    decode_token(token)
+    folder_name = str(datetime.now().strftime("%y-%m-%d_%H-%M-%S"))
+    temp_zip_path = f"{folder_name}_{file.filename}"
+    with open(temp_zip_path, "wb") as temp_zip_file:
+        content = await file.read()
+        temp_zip_file.write(content)
+    endpoint_filename = '.'.join(file.filename.split('.')[:-1])
+    zip_filename = gen_segmentations(temp_zip_path, folder_name, endpoint_filename)
+    upload_to_gcp(zip_filename, 'segmented-images')
+    return FileResponse(path=zip_filename, media_type='application/zip', filename=zip_filename)
+
+
+def gen_segmentations(file, folder_name, endpoint_filename):
+    shutil.unpack_archive(file, folder_name, 'zip')
+    upload_to_gcp(file, 'images')
+    os.remove(file)
+
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485], std=[0.229]),
+        transforms.Lambda(clamp_tensor)
+    ])
+    device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+    model = torch.load('models/best_model.pth')
+    model.eval()
+    model = model.to(device)
+
+    output_dir = f"{folder_name}_output"
+    try:
+        os.mkdir(output_dir)
+    except FileExistsError:
+        shutil.rmtree(output_dir)
+        os.mkdir(output_dir)
+    try:
+        for ext_file in os.listdir(f"{folder_name}/{endpoint_filename}"):
+            shutil.move(f"{folder_name}/{endpoint_filename}/{ext_file}", f"{folder_name}/{ext_file}")
+    except FileNotFoundError:
+        pass
+    data2predict = ProdBrainDataset(root_dir, f"{folder_name}",
+                                    transform=transform)
+    pred_loader = DataLoader(data2predict, batch_size=1, shuffle=False)
+    filenames = data2predict.img_files
+    for i, data in enumerate(pred_loader):
+        filename = filenames[i]
+        data = data.to(device)
+        pred_logits = model(data)
+        pred_binary = (pred_logits > 0.5).float()
+        mask = pred_binary.squeeze().cpu().numpy()
+        original_size = data2predict.get_original_size(i)
+        mask_resized = cv2.resize(mask, (original_size[1], original_size[0]))
+
+        mask_filename = os.path.basename(filename)
+        output_mask_path = os.path.join(output_dir, mask_filename)
+
+        mask_resized = (mask_resized * 255).astype(np.uint8)
+        cv2.imwrite(output_mask_path, mask_resized)
+
+        cv2.imwrite(output_mask_path, draw_mask_border(f"{folder_name}/{filename}", output_mask_path))
+    output_zip = f"{str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))}segmented_{endpoint_filename}_images"
+    shutil.make_archive(output_zip, 'zip', output_dir)
+    shutil.rmtree(output_dir)
+    shutil.rmtree(folder_name)
+    return f"{output_zip}.zip"
---
+++ plotly-dash-frontend/segment_images.py
@@ -0,0 +1,137 @@
+import uuid
+import dash
+import requests
+from dash import dcc, html, Input, Output, State, callback, dcc, ctx
+from app import api_url
+import dash_bootstrap_components as dbc
+import os
+import zipfile
+import io
+import base64
+import shutil
+
+from dash.exceptions import PreventUpdate
+from flask import send_file
+
+
+def create_zip(file_data_list, filenames):
+    memory_file = io.BytesIO()
+    with zipfile.ZipFile(memory_file, 'w') as zf:
+        for data, name in zip(file_data_list, filenames):
+            decoded_data = base64.b64decode(data.split(',')[1])
+            zf.writestr(name, decoded_data)
+    memory_file.seek(0)
+    return memory_file
+
+
+upload_message = 'Multiple file uploads are allowed. Please place all scans in a folder, select all files, and upload them together.'
+UPLOAD_DIRECTORY = 'uploads'
+
+layout = html.Div(style={'padding-top': '30px', 'background-image': 'url("/assets/brain_imag_bg.webp"',
+                         'height': '100vh'}, children=[
+    dbc.Row(children=[
+        dbc.Col(children=[
+            html.H2("Brain Image Tumor Segmentation", className="text-center mb-4",
+                    style={'textAlign': 'center', 'font-weight': 'bold', 'color': '#3B1C0A', 'padding-top': '10px',
+                           'font-size': '200%'}),
+        ], width=11),
+        dbc.Col(dbc.Button(id='logout', children='Logout', n_clicks=None))], justify='center'),
+    dbc.Container(style={'padding-top': '100px'}, children=[
+        dbc.Row(children=[
+            dbc.Col(children=[
+                dbc.Card([
+                    dbc.CardBody(style={'background-color': 'GhostWhite'}, children=[
+                        dbc.Label("Upload Folder", html_for="upload-files"),
+                        dbc.Card([
+                            dbc.CardBody(style={'textAlign': 'center', }, children=[
+                                dcc.Upload(id='upload-files',
+                                           children=html.Div([
+                                               'Drag and Drop or ', html.A('Select Files')]),
+                                           multiple=True,
+                                           style={'width': '100%', 'height': '60px',
+                                                  'lineHeight': '60px', 'borderWidth': '1px',
+                                                  'borderStyle': 'dashed', 'borderRadius': '5px',
+                                                  'textAlign': 'center',
+                                                  'font-family': 'Lucida Console'}), ])]),
+                        html.Br(),
+                        html.Em(upload_message, style={'color': 'green'}),
+                        html.Br(),
+                        dbc.Row(children=[dbc.Col(children=[
+                            dbc.Button("Segment Scans", id='segment', color="danger", className='text-center',
+                                       outline=True, size='md',
+                                       style={'padding-left': '45px', 'padding-right': '45px'}),
+                            dcc.Loading(dcc.Download(id='download-btn'), fullscreen=True, )
+                        ],
+                            width={'offset': 4}, style={'padding-left': '25px', 'padding-right': '25px'})],
+                            justify="center"),
+                    ], ),
+                ], ),
+            ], width={'size': 6, 'offset': 3})], style={'padding-bottom': '50px'}),
+    ], fluid=True)])
+
+
+@callback(Output('url', 'pathname'),
+          Output('token', 'data'),
+          Output('logout', 'n_clicks'),
+          Input('logout', 'n_clicks'))
+def log_out(n_clicks):
+    if n_clicks is None:
+        raise PreventUpdate
+    if n_clicks:
+        return '/', None, None
+
+
+@callback(Output('download-btn', 'data'),
+          Output('segment', 'n_clicks'),
+          Input('upload-files', 'filename'),
+          Input('upload-files', 'contents'),
+          Input('token', 'data'),
+          Input('segment', 'n_clicks'))
+def segment_images(file_names, file_contents, bearer_token, n_clicks):
+    if not n_clicks:
+        raise PreventUpdate
+    headers = {
+        'Authorization': f"Bearer {bearer_token}"
+    }
+    segment_api = f"{api_url}/prediction"
+    if n_clicks > 0 and file_contents is not None:
+        folder_name = str(uuid.uuid4())
+        folder_path = os.path.join(UPLOAD_DIRECTORY, folder_name)
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        for i, content in enumerate(file_contents):
+            content_type, content_string = content.split(',')
+            decoded_file = base64.b64decode(content_string)
+
+            with open(os.path.join(folder_path, file_names[i]), 'wb') as f:
+                f.write(decoded_file)
+
+        zip_filename = f"{folder_name}.zip"
+        zip_filepath = os.path.join(UPLOAD_DIRECTORY, zip_filename)
+
+        with zipfile.ZipFile(zip_filename, 'w') as zf:
+            for file_name in file_names:
+                zf.write(os.path.join(folder_path, file_name), file_name)
+        shutil.rmtree(folder_path)
+
+        try:
+            with open(zip_filename, "rb") as zip_file:
+                files = {
+                    "file": (zip_filename, zip_file, "application/zip")
+                }
+                response = requests.post(segment_api, headers=headers, files=files)
+                os.remove(zip_filename)
+        except FileNotFoundError:
+            return f"Error: The file {zip_filename} does not exist."
+    file_content = response.content
+    print(response.headers)
+    content_disp = response.headers.get('Content-Disposition')
+    file_name = content_disp.split('filename=')[1].strip('"')
+
+    return dcc.send_bytes(file_content, file_name), 0
+
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True, port=8051)
