74ab327d3f44f7180044eca10f24198c4cd905a4
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-10-14 02:53:39 +0000

    loading credentials as environment var; set debug to False in index for production deployment

--- plotly-dash-frontend/index.py
+++ plotly-dash-frontend/index.py
@@ -26,5 +26,6 @@ def display_page(pathname, token):
         return '404: Page not found'
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8051)
+if __name__ == "__main__":
+    app.run_server(debug=False, host="0.0.0.0", port=8051)
+
