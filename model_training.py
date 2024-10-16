fc84adc3e6a92fd60b47a1b432c46aed8eb895ba
Leonard Sanya <122167294+leonard-sanya@users.noreply.github.com>
2024-10-16 04:03:49 +0000

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -40,7 +40,7 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     root_dir = '/'
     lr = 0.001
-    num_epochs = 2
+    num_epochs = 10
     batch_size = 32
     workers = 4
 
