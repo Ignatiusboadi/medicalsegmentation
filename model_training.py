d50b4759d3d753c0428395d49a931ac2b7ac048d
Leonard Sanya <122167294+leonard-sanya@users.noreply.github.com>
2024-10-16 03:28:56 +0000

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -31,8 +31,7 @@ def main():
     os.environ['MLFLOW_TRACKING_PASSWORD'] = '67ea7e8b48b9a51dd1748b8bb71906cc5806eb09'
     os.environ['MLFLOW_TRACKING_URI'] = 'https://dagshub.com/ignatiusboadi/dagshub_proj_II.mlflow'
     
-    dagshub.login(token='67ea7e8b48b9a51dd1748b8bb71906cc5806eb09')
-    dagshub.init(repo_owner='ignatiusboadi', repo_name='dagshub_proj_II', mlflow=True)
+    dagshub.init(repo_owner='ignatiusboadi', repo_name='dagshub_proj_II',token='67ea7e8b48b9a51dd1748b8bb71906cc5806eb09', mlflow=True)
 
     mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
     mlflow.set_experiment("proj-II-model-training")
